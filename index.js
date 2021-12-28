a8779e505c71dcc22356d8590c61b9ac2020883e
Insun Yu <insunyu@Insunui-MacBookPro.local>
2021-12-28 10:56:29 +0900

    test get request method on 4000port

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi my name is insun");
+import express from "express";
+
+const app = express();
+
+const handleHome = () => console.log("Sombody is trying to get homepage!")
+
+app.get("/", handleHome)
+
+const handleListener = () => console.log("✅Your server is listening to 4000port!");
+
+app.listen(4000, handleListener);
