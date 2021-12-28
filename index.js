2a7d80571fa8fd3a40da64815794af4ce2aa1a35
Insun Yu <insunyu@Insunui-MacBookPro.local>
2021-12-28 11:36:20 +0900

    add logger middleware before each controllers

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ import express from "express";
 
 const app = express();
 
+const logger = (req,res,next) => {
+    console.log(`${req.method}:${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
-    res.send("hello world!");
+    return res.send("Welcome to the Homepage!");
 };
 
-const handleLogin = (req, res) => {
-    res.send("Login Here");
+const handlePrivateLounge = (req, res) => {
+    return res.send("Here is private Lounge");
 };
 
-app.get("/login", handleLogin);
+app.get("/private", logger, handlePrivateLounge);
 
-app.get("/", handleHome);
+app.get("/",logger, handleHome);
 
 const handleListener = () => console.log("✅Your server is listening to 4000port!");
 
