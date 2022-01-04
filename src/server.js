9ed6e0a3234071e7fc4260a73cf0aa747abad12d
Insun Yu <insunyu@Insunui-MacBookPro.local>
2022-01-04 11:22:31 +0900

    add logger middleware

--- src/server.js
+++ src/server.js
@@ -1,34 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req,res,next) => {
-    console.log(`${req.method}:${req.url}`);
-    next();
-};
-
-const protectPivate = (req,res,next) => {
-    const url = req.url;
-    if(url == "/private"){
-        return res.send("Access denied!");
-    } else {
-        console.log("Allowed!");
-        next();
-    }
-}
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
     return res.send("Welcome to the Homepage!");
 };
 
-const handlePrivateLounge = (req, res) => {
-    return res.send("Here is private Lounge");
-};
-
 app.use(logger);
-app.use(protectPivate);
-app.get("/private", handlePrivateLounge);
-app.get("/", handleHome);
+app.get("/", home);
 
 const handleListener = () => console.log("✅Your server is listening to 4000port!");
 
