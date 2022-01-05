8d631de84cbcd9271280d897fb09b8b7a4d4c220
Insun Yu <insunyu@Insunui-MacBookPro.local>
2022-01-05 16:24:29 +0900

    add loggin controller

--- src/server.js
+++ src/server.js
@@ -7,9 +7,13 @@ const logger = morgan("dev");
 const home = (req, res) => {
     return res.send("Welcome to the Homepage!");
 };
+const login = (req, res) => {
+    return res.send("Login here");
+};
 
 app.use(logger);
 app.get("/", home);
+app.get("/login", login);
 
 const handleListener = () => console.log("✅Your server is listening to 4000port!");
 
