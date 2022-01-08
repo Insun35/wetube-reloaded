8e2378a0fa0cc8d320925cc958d935026c0b0213
Insun Yu <insunyu@Insunui-MacBookPro.local>
2022-01-08 11:57:28 +0900

    add digit condition to router url

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { edit,watch, deleteVideo, upload } from "../controllers/videoController"
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id", watch);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
 
 export default videoRouter;
