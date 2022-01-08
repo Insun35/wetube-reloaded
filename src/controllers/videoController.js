c539f39f9d24ba1b7b928a0a43da57c84a272eda
Insun Yu <insunyu@Insunui-MacBookPro.local>
2022-01-08 16:55:38 +0900

    use pug views for templet

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,12 @@
-export const trendingVideos = (req, res) => res.render("home");
+export const trendingVideos = (req, res) => res.render("home", { pageTitle: "Home" });
 
 export const search = (req, res) => res.send("Search");
 
-export const watch = (req, res) => res.send(`Watch Video ${req.params.id}`);
+export const watch = (req, res) => {
+    res.render("watch", { pageTitle: "Watch", videoNumber: req.params.id});
+}
 
-export const edit = (req, res) => res.send(`Edit Video ${req.params.id}`);
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const deleteVideo = (req, res) => res.send(`Delete Video ${req.params.id}`);
 
