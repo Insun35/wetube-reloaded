e398199e2db3c1258ec340df86179ecdcf317339
Insun Yu <insunyu@Insunui-MacBookPro.local>
2022-01-11 14:02:31 +0900

    add controllers

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,9 +2,7 @@ export const trendingVideos = (req, res) => res.render("home", { pageTitle: "Hom
 
 export const search = (req, res) => res.send("Search");
 
-export const watch = (req, res) => {
-    res.render("watch", { pageTitle: "Watch", videoNumber: req.params.id});
-}
+export const watch = (req, res) => res.render("watch", { pageTitle: "Watch", videoNumber: req.params.id});
 
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
